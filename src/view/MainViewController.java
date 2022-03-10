67c2e0cf14a08a6377eb4cbdbb9dc65eefbb3b61
Marie Zephir <marie.zephir@alumni.univ-avignon.fr>
2022-03-10 11:34:59 +0100

    The first scene

--- src/view/MainViewController.java
+++ src/view/MainViewController.java
@@ -1,7 +1,33 @@
 package view;
 
+import java.util.HashMap;
+
+import ai.Coup;
+import ai.MultiLayerPerceptron;
+import ai.SigmoidalTransferFunction;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 
 public class MainViewController {
 
-	}
+    @FXML
+    private Pane sc1;
 
+    @FXML
+    private TextField textfield;
+
+    @FXML
+    private Button start;
+
+    @FXML
+    private ProgressBar pgbar;
+    
+    @FXML
+    void task(ActionEvent event) {
+    	
+    }
+}
